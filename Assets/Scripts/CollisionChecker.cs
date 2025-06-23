19079d752d124f51a30700d89cf57ca054c0c311
Odonata-Masoni <investgramvn@gmail.com>
2025-06-23 19:44:09 +0700

    Fix flipDirection cho wolf và làm mượt movement của Player

--- Assets/Scripts/CollisionChecker.cs
+++ Assets/Scripts/CollisionChecker.cs
@@ -1,68 +1,47 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class CollisionChecker : MonoBehaviour
 {
     [Header("Cấu hình kiểm tra va chạm")]
-    [SerializeField] public LayerMask collisionLayer;
-    [SerializeField] public float groundDistance = 0.2f;
-    [SerializeField] public float wallDistance = 0.4f;
-    [SerializeField] public float ceilingDistance = 0.05f;
-    [SerializeField] public float cliffDistance = 0.5f;
+    [SerializeField] private LayerMask collisionLayer;
+    [SerializeField] private float groundCheckDistance = 0.1f;
+    [SerializeField] private float ceilingCheckDistance = 0.05f;
+    [SerializeField] private float wallCheckDistance = 0.2f;
+    [SerializeField] private float cliffCheckDistance = 0.5f;
 
-    private CapsuleCollider2D touchingCol;
-    private Animator animator;
+    private CapsuleCollider2D col;
+    private ContactFilter2D filter;
     private RaycastHit2D[] hits = new RaycastHit2D[5];
-    private ContactFilter2D castFilter;
 
-    [SerializeField] private bool _isGrounded;
-    public bool IsGrounded
+    public Vector2 Direction { get; set; } = Vector2.right;
+
+    public bool IsGrounded { get; private set; }
+    public bool IsTouchingCeiling { get; private set; }
+    public bool IsTouchingWall { get; private set; }
+    public bool IsNearCliff { get; private set; }
+
+    private void Awake()
     {
-        get => _isGrounded;
-        private set { _isGrounded = value; animator?.SetBool(AnimationStrings.isGrounded, value); }
+        col = GetComponent<CapsuleCollider2D>();
+        filter.useLayerMask = true;
+        filter.layerMask = collisionLayer;
+        filter.useTriggers = false;
     }
 
-    [SerializeField] private bool _isTouchingCeiling;
-    public bool IsTouchingCeiling
+    public void Check()
     {
-        get => _isTouchingCeiling;
-        private set { _isTouchingCeiling = value; animator?.SetBool(AnimationStrings.isTouchingCeiling, value); }
-    }
+        // Ground check
+        IsGrounded = col.Cast(Vector2.down, filter, hits, groundCheckDistance) > 0;
 
-    [SerializeField] private bool _isTouchingWall;
-    public bool IsTouchingWall
-    {
-        get => _isTouchingWall;
-        private set { _isTouchingWall = value; animator?.SetBool(AnimationStrings.isTouchingWall, value); }
-    }
+        // Ceiling check
+        IsTouchingCeiling = col.Cast(Vector2.up, filter, hits, ceilingCheckDistance) > 0;
 
-    [SerializeField] private bool _isNearCliff;
-    public bool IsNearCliff
-    {
-        get => _isNearCliff;
-        private set { _isNearCliff = value; animator?.SetBool(AnimationStrings.isNearCliff, value); }
-    }
+        // Wall check (dựa trên hướng di chuyển)
+        IsTouchingWall = col.Cast(Direction.normalized, filter, hits, wallCheckDistance) > 0;
 
-    // Hướng di chuyển được cập nhật từ Wolf.cs
-    [HideInInspector] public Vector2 WalkDirectionVector = Vector2.left;
-
-    void Awake()
-    {
-        touchingCol = GetComponent<CapsuleCollider2D>();
-        animator = GetComponent<Animator>();
-
-        castFilter.useLayerMask = true;
-        castFilter.SetLayerMask(collisionLayer);
-    }
-
-    public void CheckCollisions()
-    {
-        IsGrounded = touchingCol.Cast(Vector2.down, castFilter, hits, groundDistance) > 0;
-        IsTouchingCeiling = touchingCol.Cast(Vector2.up, castFilter, hits, ceilingDistance) > 0;
-        IsTouchingWall = touchingCol.Cast(WalkDirectionVector, castFilter, hits, wallDistance) > 0;
-
-        Vector2 checkCliffPos = (Vector2)touchingCol.bounds.center + WalkDirectionVector * (touchingCol.bounds.size.x / 2 + cliffDistance);
-        IsNearCliff = !Physics2D.Raycast(checkCliffPos, Vector2.down, 2f, collisionLayer) && !IsGrounded;
-
-        Debug.Log("IsGrounded: " + IsGrounded + " | IsTouchingWall: " + IsTouchingWall + " | WalkDirection: " + WalkDirectionVector);
+        // Cliff check
+        Vector2 origin = (Vector2)col.bounds.center + Direction.normalized * (col.bounds.size.x / 2 + cliffCheckDistance);
+        IsNearCliff = !Physics2D.Raycast(origin, Vector2.down, 1f, collisionLayer) && IsGrounded;
     }
 }
