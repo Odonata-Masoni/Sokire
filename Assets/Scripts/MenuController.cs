6195c77502e7003ba6b4d626c79f43e3c5bba545
Odonata-Masoni <investgramvn@gmail.com>
2025-06-29 15:35:02 +0700

    Thêm Main Menu Scene + Button Start Game

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GamePlay Scene 1"); // Đổi tên thành đúng scene bạn dùng
+    }
+
+    //public void QuitGame()
+    //{
+    //    Application.Quit();
+    //}
+}
